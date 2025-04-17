76e14f3ef127f804afd223eb371fd2f469eea102
EUKao <corenthin.oillic44@gmail.com>
2025-04-17 14:01:58 +0200

    1st commit front

---
+++ frontend/src/components/Form/Form.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Form.module.css';
+
+const Form = () => {
+  return <h1 className={styles.title}>Form to add feedbacks</h1>;
+};
+
+export default Form;
