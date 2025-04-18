94f7ba1f950ea8a0cc4269f14c52f5799a3598c0
TomCZQ <thomasdevpro38@gmail.com>
2025-04-18 10:40:22 +0200

    fix : added CORS management

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 const peopleRoutes = require('./routes/peopleRoutes');
 const feedbackRoutes = require('./routes/feedbackRoutes');
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+require('./db/mongo'); 
+
 const port = 3001;
 
 app.use(express.json());
 
-app.use('/peoples', peopleRoutes);
-app.use('/feedbacks', feedbackRoutes);
+app.use(
+    cors({
+      origin: [process.env.FRONT_URL, "*"],
+      methods: ["GET", "POST", "DELETE"],
+      allowedHeaders: "*"
+    })
+  );
+
+app.use('/api', peopleRoutes);
+app.use('/api', feedbackRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
